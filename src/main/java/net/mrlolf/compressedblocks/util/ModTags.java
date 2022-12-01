3c8f26ee3f52fe57af455e572c41acc471b9042c
Fabian Roscher <roscher2003@outlook.de>
2022-12-01 18:48:21 +0100

    Back ported to 1.17

--- src/main/java/net/mrlolf/compressedblocks/util/ModTags.java
+++ src/main/java/net/mrlolf/compressedblocks/util/ModTags.java
@@ -1,30 +1,28 @@
 package net.mrlolf.compressedblocks.util;
 
+import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.tag.TagKey;
+import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import net.mrlolf.compressedblocks.CompressedBlocks;
 
 public class ModTags {
     public class Blocks{
-        public static final TagKey<Block> createTag(String name){
-            return TagKey.of(Registry.BLOCK_KEY, new Identifier(CompressedBlocks.MOD_ID, name));
+        public static final Tag.Identified<Block> createTag(String name){
+            return (Tag.Identified<Block>) TagRegistry.block(new Identifier(CompressedBlocks.MOD_ID, name));
         }
-        public static final TagKey<Block> createCommonTag(String name){
-            return TagKey.of(Registry.BLOCK_KEY, new Identifier("c", name));
+        public static final Tag.Identified<Block> createCommonTag(String name){
+            return (Tag.Identified<Block>) TagRegistry.block(new Identifier("c", name));
         }
     }
 
     public class Items{
-
-        public static final TagKey<Item> createTag(String name){
-            return TagKey.of(Registry.ITEM_KEY, new Identifier(CompressedBlocks.MOD_ID, name));
+        public static final Tag.Identified<Item> createTag(String name){
+            return (Tag.Identified<Item>) TagRegistry.item(new Identifier(CompressedBlocks.MOD_ID, name));
         }
-        public static final TagKey<Item> createCommonTag(String name){
-            return TagKey.of(Registry.ITEM_KEY, new Identifier("c", name));
+        public static final Tag.Identified<Item> createCommonTag(String name){
+            return (Tag.Identified<Item>) TagRegistry.item(new Identifier("c", name));
         }
-
     }
 }
